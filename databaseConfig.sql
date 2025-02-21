8552330fad4dc5fa188cde4bde73e45465ad9398
Nick Greco <33033027+NMG888@users.noreply.github.com>
2025-02-21 08:32:39 -0700

    Database config file update

--- databaseConfig.sql
+++ databaseConfig.sql
@@ -5,10 +5,11 @@ CREATE DATABASE CS440;
 use CS440;
 
 CREATE TABLE events_schedule (
-    event_name VARCHAR(255) NOT NULL,
+    event_name VARCHAR(35) NOT NULL,
     event_day ENUM('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday') NOT NULL,
     event_start_time TIME NOT NULL,
-    event_end_time TIME NOT NULL
+    event_end_time TIME NOT NULL,
+    PRIMARY KEY (event_name)
 );
 
 CREATE TABLE Login (
@@ -17,7 +18,7 @@ CREATE TABLE Login (
 );
 
 #for testing
-insert into Login(username, pass) VALUES ("nmg295", "password1234");
+#insert into Login(username, pass) VALUES ("nmg295", "password1234");
 
-select * from events_schedule;
-select * from Login;
+#select * from events_schedule;
+#select * from Login;
